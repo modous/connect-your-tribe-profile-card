117af01ea45255e6288d8a5cb1596c484b005ead
modous <mohamed1010@live.nl>
2023-02-13 09:23:24 +0100

    versie 1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { response } from 'express'
 
+const url = 'https://whois.fdnd.nl/api/v1/member/mohamed-taissate'
+const data = await fetch(url).then((response) => response.json())
+
+console.log(data)
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,7 +18,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
